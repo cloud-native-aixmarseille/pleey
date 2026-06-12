b8787ba550c2ab7d1749741fff662d954e5be6a6
Emilien Escalle <emilien.escalle@escemi.com>
2026-06-12 22:20:34 +0200

    fix(identity): user avatar id type issue
    Signed-off-by: Emilien Escalle <emilien.escalle@escemi.com>

--- application/backend/src/presentation/identity/http/avatar-controller.ts
+++ application/backend/src/presentation/identity/http/avatar-controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Param, ParseIntPipe, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import type { Response } from 'express';
 import { GetGuestAvatarPreviewUseCase } from '../../../application/identity/avatar/use-cases/get-guest-avatar-preview-use-case';
 import { GetGuestAvatarUseCase } from '../../../application/identity/avatar/use-cases/get-guest-avatar-use-case';
 import { GetUserAvatarUseCase } from '../../../application/identity/avatar/use-cases/get-user-avatar-use-case';
-import type { UserId } from '../../../domain/identity/entities/user';
+import { UserIdentifier } from '../../../application/identity/shared/services/identifiers/user-identifier';
 
 @Controller('api/avatars')
 export class AvatarController {
@@ -11,14 +11,12 @@ export class AvatarController {
     private readonly getUserAvatarUseCase: GetUserAvatarUseCase,
     private readonly getGuestAvatarPreviewUseCase: GetGuestAvatarPreviewUseCase,
     private readonly getGuestAvatarUseCase: GetGuestAvatarUseCase,
+    private readonly userIdentifier: UserIdentifier,
   ) {}
 
   @Get('users/:userId')
-  async getUserAvatar(
-    @Param('userId', ParseIntPipe) userId: UserId,
-    @Res() res: Response,
-  ): Promise<void> {
-    const avatar = await this.getUserAvatarUseCase.execute(userId);
+  async getUserAvatar(@Param('userId') userId: string, @Res() res: Response): Promise<void> {
+    const avatar = await this.getUserAvatarUseCase.execute(this.userIdentifier.parse(userId));
 
     res.setHeader('Content-Type', avatar.mimeType);
     res.setHeader('Cache-Control', 'no-store, max-age=0, must-revalidate');
